51e803cf29cb8e9e455e74a8d7d2154e76b9bfaf
bahadir <baha_i@hotmail.com>
2021-10-19 09:23:03 +0200

    für divs foreach funct  erstellt

--- main.js
+++ main.js
@@ -17,6 +17,55 @@ const game = () => {
         squares.textContent= i + 1;
         section.appendChild(squares);
     }
-    
+    document.querySelectorAll("div").forEach(item=>{
+        item.style.pointerEvents = "none";
+    });
+    setTimeout(() => {
+        document.querySelectorAll("div").forEach(item => {
+            // item.style.color="transparent";
+            item.style.pointerEvents="all";
+        })
+    }, 1000);
+
+    document.querySelectorAll("div").forEach((item) => {
+        item.addEventListener("click", (e) => { 
+            e.target.style.color = "white";
+
+            if(e.target.textContent!= index){
+               e.target.style.background="red";
+               wrong.play(); 
+               wrong.currentTime = 0 ;
+               document.querySelectorAll("div").forEach((item) => {
+                item.style.pointerEvents = "none";
+
+            });
+            if(level!=1){
+                setTimeout(() => {
+                    level--;
+                    index = 1 ;
+                    game();
+                },2000);
+            } else {
+                setTimeout(() => {
+                index = 1 ;
+                game();
+            },2000);
+        }
+        } 
+        else { 
+            e.target.style.background = "green";
+        correct.play();
+        correct.currentTime = 0 ;
+        if(index=== level+1){
+            setTimeout(() => {
+                level++;
+                index = 1;
+                game();
+            },2000);
+        }
+        index++;
+    }
+    });
+  });
 }
 game();
