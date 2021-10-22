c62de183c75b8c5c543049651fa95293837bebc2
bahadir <baha_i@hotmail.com>
2021-10-22 21:33:25 +0200

    writehederinfo funct erstellt

--- main.js
+++ main.js
@@ -13,16 +13,38 @@ const game = () => {
     let yList = [];
 
     section.innerHTML = "";
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    // 30 width + border 5px sag ve sol = 40
+    let xRandom = (width * 80 / 100) - 40;
+    const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+    let yRandom = (height * 80 / 100) - 40;
+
+    writeHederinfo(level +"."+ "level basliyor");
     for (let i=0; i< level+1; i++) {
         let squares = document.createElement("div");
         squares.classList.add("square");
-        let x = Math.floor(Math.random()*50);
-        let y = Math.floor(Math.random()*70);
-        console.log(x,y)
-        squares.style.top = y + "%";
-        squares.style.left = x + "%";
-        squares.textContent= i + 1;
-        section.appendChild(squares);
+
+        // bu kisim karelerin kesismesi halinde yeniden x, y degeri atamak icin yazilmistir
+        let whileCondition = true;
+        while (whileCondition) {
+            whileCondition = false;
+            let x = Math.floor(Math.random()* xRandom);
+            let y = Math.floor(Math.random()* yRandom);
+            if(checkFark(x,y, xList, yList)){
+                whileCondition = true;
+                console.log("ayni geldi")
+            } else {
+                xList.push(x);
+                yList.push(y);
+                squares.style.top = y + "px";
+                squares.style.left = x + "px";
+                // height ve weight css de 30px e ayarlandi
+                squares.textContent= i + 1;
+                section.appendChild(squares);
+                whileCondition = false;
+            }
+        }
+
     }
     document.querySelectorAll("div").forEach(item=>{
         item.style.pointerEvents = "none";
